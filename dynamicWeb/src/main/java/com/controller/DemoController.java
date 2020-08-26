fd548fa16a4d5a9ddaf7e8015d04787607fbdfe6
zlshen <1129120844@qq.com>
2020-08-26 17:39:36 +0800

    提交测试controller文件

--- dynamicWeb/src/main/java/com/controller/DemoController.java
+++ dynamicWeb/src/main/java/com/controller/DemoController.java
@@ -8,4 +8,7 @@ public class DemoController {
     public static void main(String[] args) {
         System.out.println("测试Git的提交文件的使用。");
     }
+    public void testCommit() {
+        System.out.println("测试IDEA的提交操作！");
+    }
 }
